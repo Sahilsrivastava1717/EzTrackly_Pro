2a57b85203f4095d74c1f9179960eaa9c79e8aa6
Sahil Srivastava <sahilsrivastava1717@gmail.com>
2026-06-03 11:23:15 +0530

    Fix routing and remove app redirect

--- eztrackly/next.config.mjs
+++ eztrackly/next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
-
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/app",
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
